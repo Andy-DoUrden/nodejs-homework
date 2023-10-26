3fd30ac5c370e812cfeedd3dabee807ab117fa35
Andy-DoUrden <111894438+Andy-DoUrden@users.noreply.github.com>
2023-10-27 00:55:42 +0300

    Completed homework

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -3,11 +3,15 @@ const Joi = require("joi");
 const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "org", "ca", "uk"] } }),
-  phone: Joi.string()
-    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
-    .required(),
+  phone: Joi.string().pattern(/^\(\d{3}\) \d{3}-\d{4}$/),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 module.exports = {
   addSchema,
+  updateFavoriteSchema,
 };
